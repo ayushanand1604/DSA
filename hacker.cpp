e01228a5bb655f05cf84c22b5f9441b3b46f73c6
Ayush Anand <ayushanand@Ayushs-MacBook-Air.local>
2025-06-05 02:56:17 +0530

    hey

--- hacker.cpp
+++ hacker.cpp
@@ -421,3 +421,53 @@ int main() {
 	cout<<count2;
 
 }
+// Sequence equation
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    int p[n];
+    for(int i=0;i<=n;i++){
+        cin>>p[i];
+    }
+    int c;
+    for(int a=1;a<=n;a++){
+        for(int i=0;i<n;i++){
+            if(p[i]==a){
+                    c=i+1;}}
+        for(int i=0;i<n;i++){
+            if(p[i]==c){
+                
+                cout<<i+1<<endl;
+            }
+            
+        }
+        }         
+        }
+// LIBRARY FINE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int d1,m1,y1;
+    cin>>d1>>m1>>y1;
+    int d2,m2,y2;
+    cin>>d2>>m2>>y2;
+    int fine=0;
+    if(y1>y2){
+        fine=10000;
+    } 
+    else if(y1==y2){
+    
+    if((d1>d2)&&(m1==m2)){
+        fine=15*(d1-d2);
+    }
+    else if(m1>m2){
+        fine=500*(m1-m2);
+    }
+    }
+    cout<<fine;
+}
