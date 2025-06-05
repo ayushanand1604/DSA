704d8f707f95148ee85819f894e1bb8666096a49
Ayush Anand <ayushanand@192.168.0.108>
2025-06-05 20:25:42 +0530

    hii

--- hacker.cpp
+++ hacker.cpp
@@ -471,3 +471,25 @@ int main() {
     }
     cout<<fine;
 }
+// DAY OF THE PROGRAMMER
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int y;
+   cin>>y;
+   if(y==1918){
+    cout<<"26.09.1918";
+   }
+   else if(y<1918){
+    if(y%4==0){cout<<"12.09."<<y;}
+    else{cout<<"13.09."<<y;}
+   }
+    else{
+        if((y%400==0)||(y%4==0&&y%100!=0))
+        cout<<"12.09."<<y;
+        else {
+            cout<<"13.09."<<y;
+        }
+   }
+}
