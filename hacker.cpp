737cc1dfa4e76a37e471b6a1f09b60fa2ed16e36
Ayush Anand <ayushanand@192.168.0.103>
2025-07-20 01:05:11 +0530

    hii

--- hacker.cpp
+++ hacker.cpp
@@ -777,3 +777,23 @@ int main() {
     cout<<count;
     return 0;
 }
+// Circular Array Rotation
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,k,q;
+    cin >>n>>k>>q;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    k=k%n;
+    for(int i=0;i<q;i++){
+        int queris;
+        cin>>queris;
+        int a=(queris-k+n)%n;
+        cout<<arr[a]<<endl;
+    }
+    return 0;
+}
