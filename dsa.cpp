347230764b76ee6394e8cb9147d11433ffe130a0
Ayush Anand <ayushanand1604@gmail.com>
2025-10-24 18:14:45 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -779,3 +779,23 @@ class Solution {
         return ans;
     }
 };
+// leetcode maximum consecutive ones problem
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int max=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+                if(count>max){
+                    max=count;
+                }
+            }
+            else{
+                count=0;
+            }
+        }
+        return max;
+    }
+};
