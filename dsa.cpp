f395e1ed431538c6dc4cc5fa6ca63a0369ddcd4f
Ayush Anand <ayushanand1604@gmail.com>
2025-11-21 20:00:09 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -149,8 +149,6 @@ int main(){
 //     return 0;
 // }
 
-
-
 // #include <bits/stdc++.h>
 // using namespace std;
 // int main(){
@@ -950,3 +948,26 @@ public:
     }
     return low; 
     }
+    // Two sum leetcode 0(n)
+    class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<pair<int,int>> ans(n);
+        for(int i=0;i<n;i++){
+            ans[i]={nums[i],i};
+        }
+        sort(ans.begin(),ans.end());
+        int i=0;
+        int j=nums.size()-1;
+        while(i<j){
+            if(ans[i].first+ans[j].first==target){
+
+                return{ans[i].second,ans[j].second};
+            }
+            else if(ans[i].first+ans[j].first>target) j--;
+            else if(ans[i].first+ans[j].first<target) i++;
+        }
+        return {};
+    }
+};
