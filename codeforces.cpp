7e6e59862ab4e83131774e60b5995ce91af90726
Ayush Anand <ayushanand1604@gmail.com>
2025-11-28 17:02:27 +0530

    hello

--- codeforces.cpp
+++ codeforces.cpp
@@ -8,8 +8,38 @@ int main() {
     while (t--) {
         long long n, k;
         cin >> n >> k;
-        long long ans = k + (k - 1) / (n - 1);
+        long long ans = k + ((k - 1) / (n - 1));
         cout << ans <<endl;
     }
     return 0;
+}
+// sum of round numbers
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+        int n;
+        vector<int> ans;
+        cin>>n;
+        int place = 1;
+        int count=0;
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit != 0) {
+                count++;
+                ans.push_back(digit*place);
+            }
+            n=n/10;
+            place=place*10;
+        }
+    cout<<count<<endl;
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    }
 }
