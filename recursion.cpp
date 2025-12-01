b81153392c0161f269769e59840f8def6c1587a1
Ayush Anand <ayushanand1604@gmail.com>
2025-12-01 21:38:28 +0530

    hello

--- recursion.cpp
+++ recursion.cpp
@@ -109,3 +109,15 @@ class Solution {
         cout<<n<<" ";
     }
 };
+// print N to 1 using recursive function gfg
+class Solution {
+  public:
+    void printNos(int N) {
+        // code here
+        if(N==0){
+            return;
+        }
+        cout<<N<<" ";
+        printNos(N-1);
+    }
+};
