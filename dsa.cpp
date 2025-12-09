af57da96132c23d908991f556bccd9a4832e764b
Ayush Anand <ayushanand1604@gmail.com>
2025-12-09 20:20:36 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -993,3 +993,21 @@ public:
         return ans;
     }
 };
+// leetcode length of lastword string
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]==' '){
+                if(count>0){
+                    return count;
+                }
+            }
+            else{
+                count++;
+            }
+        }
+        return count;
+    }
+};
