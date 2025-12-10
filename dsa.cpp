cf6d50c355e95c68b95e0f8f30bf8a14d63600a6
Ayush Anand <ayushanand1604@gmail.com>
2025-12-10 23:29:49 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1056,3 +1056,19 @@ public:
         return ans;
     }
 };
+// climbing stairs leetcode
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        int a=1,b=2;
+        for(int i=3;i<=n;i++){
+            int t=a+b;
+            a=b;
+            b=t;
+        }
+        return b;
+    }
+};
