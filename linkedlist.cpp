ff5dede4ccffdf1c71a94a124aa2c8745ab82901
Ayush Anand <ayushanand1604@gmail.com>
2025-12-15 20:23:44 +0530

    hii

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -209,3 +209,33 @@ public:
         return slow;
     }
 };
+// delete middle of linked list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head==nullptr||head->next==nullptr){
+            return nullptr;
+        }
+        ListNode *fast=head;
+        ListNode *slow=head;
+        ListNode *dummy=nullptr;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast=fast->next->next;
+            dummy=slow;
+            slow=slow->next;
+        }
+        dummy->next=slow->next;
+        delete slow;
+        return head;
+    }
+};
