41fd5c13f61ea6a6656b23826846a13a99349d2b
Ayush Anand <ayushanand1604@gmail.com>
2025-12-15 18:25:24 +0530

    hii

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -196,3 +196,16 @@ public:
     }
 
 };
+// middle of the linked list leetcode
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
+};
