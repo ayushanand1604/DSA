4495650a4fa1682a30e1052d4e83caba89d92da5
Ayush Anand <ayushanand1604@gmail.com>
2025-12-16 22:05:21 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1113,3 +1113,27 @@ public:
         return y;
     }
 };
+// assign cookies
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n=g.size();
+        int m=s.size();
+        int count=0;
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0;
+        int j=0;
+        while(i<g.size()&&j<s.size()){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return count;
+    }
+};
