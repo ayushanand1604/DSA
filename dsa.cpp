b48f3c574e5f24a6928b5ce746206f99e4389d12
Ayush Anand <ayushanand1604@gmail.com>
2025-12-21 22:18:57 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1220,3 +1220,19 @@ public:
         return {repeating,missing};
     }
 };
+// first unique character in string leetcode
+ class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int>m;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(m[s[i]]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
