ee1151015d586e968c1de37f700227c5bdbdb761
Ayush Anand <ayushanand1604@gmail.com>
2025-12-22 23:18:15 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1246,3 +1246,19 @@ public:
         reverse(nums.begin()+k,nums.end());
     }
 };
+// Number of changing keys
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        for(int i=0;i<s.length();i++){
+            s[i]=tolower((unsigned char)s[i]);
+        }
+        int count=0;
+        for(int i=0;i<s.length()-1;i++){
+            if(s[i]!=s[i+1]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
