10ab2407e3c553dd4adc0da1c35ca624645eff0a
Ayush <ayushanand1604@gmail.com>
2025-12-25 00:00:45 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1305,4 +1305,15 @@ class Solution {
         return ans;
     }
 };
+// XOR operations in an array leetcode
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans^=start+2*i;
+        }
+        return ans;
+    }
+};
 
