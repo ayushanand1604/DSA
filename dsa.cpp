85fa87d492b0750ed75b38eb6be4edff3f055598
Ayush Anand <ayushanand1604@gmail.com>
2025-12-27 23:08:01 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1316,4 +1316,17 @@ public:
         return ans;
     }
 };
+// first string palindrome in string
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(int i=0;i<words.size();i++){
+            string temp=words[i];
+            reverse(temp.begin(),temp.end());
+            if(temp==words[i])
+                return words[i];
+            }
+        return "";
+    }
+};
 
