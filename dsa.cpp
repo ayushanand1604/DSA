e7d9451c6bda510fe6535df31bf2fcb04009d333
Ayush Anand <ayushanand1604@gmail.com>
2025-12-28 23:50:54 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1341,4 +1341,21 @@ public:
         return s;
     }
 };
-
+// Minimum in rotated sorted array leetcode
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int high=nums.size()-1;
+        int low=0;
+        while(low<high){
+            int mid=(high-low)/2+low;
+            if(nums[mid]>=nums[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return nums[low];
+    }
+};
