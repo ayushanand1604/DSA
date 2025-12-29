9b4fc849dc32b5b19cd9236da997f7597e38bea9
Ayush Anand <ayushanand1604@gmail.com>
2025-12-29 21:45:18 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1359,3 +1359,18 @@ public:
         return nums[low];
     }
 };
+// sum of multiples leetcode
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        if(n == 1 || n == 2) return 0;
+        if(n == 3) return n;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0||i%5==0||i%7==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+};
