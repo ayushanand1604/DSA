10e365769a4117210da1bea33f346dfcd02ff451
Ayush <ayushanand1604@gmail.com>
2026-01-13 17:19:26 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1503,3 +1503,27 @@ public:
         return false;    
     }
 };
+// remove element from sorted array || leetcode
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count=1;
+        int i=1;
+        while(i<nums.size()){
+            if(nums[i]==nums[i-1]){
+                count++;
+                if(count>2){
+                    nums.erase(nums.begin()+i);
+                }    
+                else{
+                    i++;
+                }
+            }
+            else{
+                count=1;
+                i++;
+            }
+        }
+        return nums.size();
+    }
+};
