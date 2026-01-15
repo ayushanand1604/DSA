ece33c1e1ae1b0ec2fe5eb95e9fc403852338c5b
Ayush Anand <ayushanand1604@gmail.com>
2026-01-15 21:25:42 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1527,3 +1527,21 @@ public:
         return nums.size();
     }
 };
+// peak index in a mountain array leetcode
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low=0;
+        int high=arr.size()-1;
+        while(low<high){
+            int mid=((high-low)/2)+low;
+            if(arr[mid]<arr[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+};
