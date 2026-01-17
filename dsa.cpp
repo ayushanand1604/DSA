eb7cc126b0fd7832d108ef7c15c28711c7d72c6b
Ayush <ayushanand1604@gmail.com>
2026-01-17 15:15:31 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1545,3 +1545,21 @@ public:
         return low;
     }
 };
+// Maximum subarray of size k
+class Solution {
+  public:
+    int maxSubarraySum(vector<int>& arr, int k) {
+        // code here
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+        int maxSum=sum;
+        for(int i=k;i<arr.size();i++){
+            sum=sum+arr[i]-arr[i-k];
+            if(sum>maxSum)
+                maxSum=sum;
+        }
+        return maxSum;
+    }
+};
