07b903f280047c518274b73cb11d09e036a1e4a7
Ayush <ayushanand1604@gmail.com>
2026-01-19 20:43:40 +0530

    hii

--- dsa.cpp
+++ dsa.cpp
@@ -1563,3 +1563,22 @@ class Solution {
         return maxSum;
     }
 };
+// 167. Two Sum II - Input Array Is Sorted leetcode
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0;
+        int j=numbers.size()-1;
+        vector<int>ans;
+        while(i<=j){
+            if(numbers[i]+numbers[j]==target){
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                return ans;
+            }
+            else if(numbers[i]+numbers[j]>target) j--;
+            else i++;
+        }
+        return {};
+    }
+};
