c3312426f11bf1133a311fc8d4c0f6e9ebce1dfb
Ayush <ayushanand1604@gmail.com>
2026-01-20 23:43:26 +0530

    running sum

--- dsa.cpp
+++ dsa.cpp
@@ -1634,3 +1634,16 @@ class Solution {
         return maxm;
     }
 };
+// Running Sum of 1d Array leetcode
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int>ans;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
