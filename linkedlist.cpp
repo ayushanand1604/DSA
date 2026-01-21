d51422b8790d80d0ea7394589faf1d47e4c311cd
Ayush <ayushanand1604@gmail.com>
2026-01-21 13:23:19 +0530

    linked list detect loop

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -283,3 +283,34 @@ class Solution {
         return head;
     }
 };
+// detect loop in linked list gfg floyd cycle
+*
+class Node {
+   public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+} */
+class Solution {
+  public:
+    bool detectLoop(Node* head) {
+        // code here
+        Node* slow=head;
+        Node* fast=head;
+        if(head==NULL){
+            return false;
+        }
+        while(slow!=NULL&&fast!=NULL&&fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;;
+    }
+};
