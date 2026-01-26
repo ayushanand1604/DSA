768f50c36b068e64560fd688671cf162db785112
Ayush <ayushanand1604@gmail.com>
2026-01-26 15:14:19 +0530

    sliding window attempt

--- dsa.cpp
+++ dsa.cpp
@@ -1737,3 +1737,25 @@ public:
         return count;
     }
 };
+// 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold leetcode 
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int count=0;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+        if((sum/k)==threshold||(sum/k)>threshold){
+            count++;
+        }
+        for(int i=k;i<arr.size();i++){
+            sum+=arr[i];
+            sum-=arr[i-k];
+            if((sum/k)==threshold||(sum/k)>threshold){
+                count++;
+            }
+        }
+        return count;
+    }
+};
