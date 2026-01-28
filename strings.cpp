c6b4e70bee614df5c29f096773c69b0314a7dde9
Ayush <ayushanand1604@gmail.com>
2026-01-29 00:25:10 +0530

    string using stack

--- strings.cpp
+++ strings.cpp
@@ -164,3 +164,24 @@ public:
         return false;
     }
 };
+// 20. Valid Parentheses leetcode
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if (s[i]=='('||s[i]=='['||s[i]=='{'){
+                st.push(s[i]);
+            }
+            else {
+                if(st.empty()) return false;
+                char prev = st.top();
+                st.pop();
+                if(s[i]==')'&&prev!='(') return false;
+                if (s[i]==']'&&prev!='[') return false;
+                if (s[i]=='}'&&prev!='{') return false;
+            }
+        }
+        return st.empty();
+    }
+};
