452cdf80fe7ad3212b237c0c44cdf7372d1235c9
Ayush <ayushanand1604@gmail.com>
2026-01-28 20:08:27 +0530

    robot return string

--- strings.cpp
+++ strings.cpp
@@ -148,3 +148,19 @@ public:
         return result;
     }
 };
+// 657. Robot Return to Origin leetcode
+class Solution {
+public:
+    bool judgeCircle(string moves){
+        int x=0;
+        int y=0;
+        for(int i=0;i<moves.size();i++){
+            if(moves[i]=='U') y++;
+            else if(moves[i]=='D') y--;
+            else if(moves[i]=='L') x--;
+            else if(moves[i]=='R') x++;
+        }
+        if(x==0&&y==0) return true;
+        return false;
+    }
+};
