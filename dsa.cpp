c100bad7439cb09f08c646723d698e684679eaed
Ayush <ayushanand1604@gmail.com>
2026-01-31 01:05:29 +0530

    binary search sqrt leetcode 69

--- dsa.cpp
+++ dsa.cpp
@@ -1779,3 +1779,24 @@ class Solution {
         return false;
     }
 };
+// sqrt through binary search leetcode 69
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+        int low=1;
+        int high=x;
+        int ans=0;
+        while(low<=high){
+            int mid=((high-low)/2)+low;
+            if(mid <= x / mid){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
