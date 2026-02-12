5be65bba644cf0f91168b0ce033df9ac9f8d716b
Ayush <ayushanand1604@gmail.com>
2026-02-12 19:00:39 +0530

    add two numbers linked list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -345,3 +345,27 @@ class Solution {
         return head;
     }    
 };
+// 2.Add two numbers leetcode
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans=new ListNode();
+        ListNode* cur=ans;
+        int carry=0;
+        while(l1!=nullptr||l2!=nullptr||carry!=0){
+            int sum=carry;
+            if(l1!=nullptr){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=nullptr){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            carry=sum/10;
+            cur->next=new ListNode(sum%10);
+            cur=cur->next;
+       }
+       return ans->next;
+    }
+};
