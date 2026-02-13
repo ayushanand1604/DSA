4e3a602085a859d8d9adba02a020a7ef4f94add4
Ayush <ayushanand1604@gmail.com>
2026-02-14 00:49:45 +0530

    reverse of string using two pointers

--- strings.cpp
+++ strings.cpp
@@ -185,3 +185,16 @@ public:
         return st.empty();
     }
 };
+// Reverse of string leetcode 344
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+    }
+};
