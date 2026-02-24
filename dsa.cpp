e0b4fd3b90abdb65307909c3908016bfc8948076
Ayush <ayushanand1604@gmail.com>
2026-02-24 14:42:26 +0530

    happy number

--- dsa.cpp
+++ dsa.cpp
@@ -1878,3 +1878,22 @@ public:
         return sum;
     }
 };
+// happy number leetcode
+class Solution {
+public:
+    bool isHappy(int n) {
+        set<int>s;
+        while(n!=1){
+            if(s.count(n)) return false;
+            s.insert(n);
+            int sum=0;
+            while(n>0){
+                int digit=n%10;
+                sum+=digit*digit;
+                n=n/10;
+            }
+            n=sum;
+        }
+        return true;
+    }
+};
