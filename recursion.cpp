642d5d7ff423f78662aa3930cb6f8f9589421643
Ayush <ayushanand1604@gmail.com>
2026-03-01 09:51:19 +0530

    powe of two using recursion

--- recursion.cpp
+++ recursion.cpp
@@ -188,3 +188,13 @@ public:
         return isPowerOfFour(n/4);
     }
 };
+// 231. Power of Two leetcode using recursion
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        if(n==1) return true;
+        if(n%2!=0) return false;
+        return isPowerOfTwo(n/2);
+    }
+};
