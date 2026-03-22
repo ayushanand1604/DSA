1729d1c3512296e08c570f236f476b95f66f8b9b
Ayush <ayushanand1604@gmail.com>
2026-03-22 23:46:41 +0530

    binary search

--- dsa.cpp
+++ dsa.cpp
@@ -2010,3 +2010,17 @@ public:
         return startIndex;
     }
 };
+// 162. Find Peak Element leetcode 
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<high){
+            int mid=(high+low)/2;
+            if(nums[mid]<nums[mid+1])low=mid+1;
+            else high=mid;
+        }
+        return low;
+    }
+};
