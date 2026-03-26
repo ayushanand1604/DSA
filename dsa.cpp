57d5acab06780ff6ec50599753815cc981c785e5
Ayush <ayushanand1604@gmail.com>
2026-03-26 18:06:53 +0530

    maximum 69

--- dsa.cpp
+++ dsa.cpp
@@ -2118,3 +2118,18 @@ public:
         return count;
     }
 };
+// 1323. Maximum 69 Number leetcode
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string ans=to_string(num);
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]=='6'){
+                ans[i]='9';
+                break;
+            }
+        }
+        int result=stoi(ans);
+        return result;
+    }
+};
