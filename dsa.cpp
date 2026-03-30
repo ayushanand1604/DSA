ced35f20776ebef5d579ffdac03ec80171f140ce
Ayush <ayushanand1604@gmail.com>
2026-03-30 22:32:53 +0530

    two pointers easy

--- dsa.cpp
+++ dsa.cpp
@@ -2155,3 +2155,26 @@ public:
         }
     }
 };
+// 922. Sort Array By Parity II leetcode
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int i=0;
+        int j=1;
+        int n=nums.size();
+        while(i<n&&j<n){
+            if(nums[i]%2==0){
+                i+=2;
+            }
+            else{
+                while(nums[j]%2==1){
+                    j+=2;
+                }
+                swap(nums[i],nums[j]);
+                i+=2;
+                j+=2;
+            }
+        }
+        return nums;
+    }
+};
