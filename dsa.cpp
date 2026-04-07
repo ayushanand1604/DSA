1fc0995df8df9c4a6cfc38d8f1d3730ef24c9d47
Ayush <ayushanand1604@gmail.com>
2026-04-07 22:05:46 +0530

    sliding window with map

--- dsa.cpp
+++ dsa.cpp
@@ -2321,3 +2321,27 @@ public:
         return false;
     }
 };
+// 904. Fruit Into Baskets leetcode
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n=fruits.size();
+        int i=0;
+        int j=0;
+        unordered_map<int,int>mpp;
+        int count=0;
+        while(j<n){
+            mpp[fruits[j]]++;
+            if(mpp.size()<=2){
+                count=max(count,j-i+1);
+            }
+            else{
+                mpp[fruits[i]]--;
+                if(mpp[fruits[i]]==0) mpp.erase(fruits[i]);
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+};
