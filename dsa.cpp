465e8e301e9298c03b372eb36247bca1e9d859f2
Ayush <ayushanand1604@gmail.com>
2026-04-21 18:27:44 +0530

    max heap priority queue 215

--- dsa.cpp
+++ dsa.cpp
@@ -2471,3 +2471,18 @@ public:
         return total;
     }
 };
+215. Kth Largest Element in an Array
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>q;
+        for(int i=0;i<nums.size();i++){
+            q.push(nums[i]);
+        }
+        for(int i=0;i<k;i++){
+            if(i==k-1) return q.top();
+            else q.pop();
+        }
+        return -1;
+    }
+};
