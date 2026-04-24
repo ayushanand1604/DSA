c71f7ce8dfd8342fd3a34cd3f7178e91230835ae
Ayush <ayushanand1604@gmail.com>
2026-04-24 17:38:44 +0530

    dp

--- dsa.cpp
+++ dsa.cpp
@@ -2585,3 +2585,18 @@ public:
         }
     }
 };
+2833. Furthest Point From Origin
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int left=0;
+        int right=0;
+        int dash=0;
+        for(int i=0;i<moves.length();i++){
+            if(moves[i]=='L') left++;
+            else if(moves[i]=='R') right++;
+            else dash++;
+        }
+        return abs(left-right)+dash;
+    }
+};
