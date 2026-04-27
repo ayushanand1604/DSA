1fe0ab7353788c49657ccaf7b37b58e6adc39bb5
Ayush <ayushanand1604@gmail.com>
2026-04-27 16:46:46 +0530

    leetcode 238 optimal

--- dsa.cpp
+++ dsa.cpp
@@ -2621,3 +2621,20 @@ public:
         return ans;
     }
 };
+238. Product of Array Except Self
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>ans(n,1);
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]*arr[i-1];
+        }
+        int rightProduct=1;
+        for(int i=n-2;i>=0;i--){
+            rightProduct*=arr[i+1];
+            ans[i]*=rightProduct;
+        }
+        return ans;
+    }
+};
