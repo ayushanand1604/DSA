94b372a8144b293da07c7799c585bdd6e3f753f0
Ayush <ayushanand1604@gmail.com>
2026-04-30 22:16:37 +0530

    leetcode 328 linked list odd even

--- dsa.cpp
+++ dsa.cpp
@@ -2638,3 +2638,31 @@ public:
         return ans;
     }
 };
+328. Odd Even Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head||!head->next) return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
+        ListNode* evenHead=even;
+        while(even!=nullptr&&even->next!=nullptr){
+            odd->next=even->next;
+            odd=odd->next;
+            even->next=odd->next;
+            even=even->next;
+        }
+        odd->next=evenHead;
+        return head;
+    }
+};
