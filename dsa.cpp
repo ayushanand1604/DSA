0a1a0977bb2eece4eea515a3d6ef0fb27925c482
Ayush <ayushanand1604@gmail.com>
2026-05-01 21:45:41 +0530

    leetcode string 28

--- dsa.cpp
+++ dsa.cpp
@@ -2683,3 +2683,17 @@ public:
         return swap;
     }
 };
+28. Find the Index of the First Occurrence in a String
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.size()>haystack.size()) return -1;
+        int n=needle.size();
+        int m=haystack.size();
+        for(int i=0;i<=m-n;i++){
+            string s=haystack.substr(i,n);
+            if(s==needle) return i;
+        }
+        return -1;
+    }
+};
