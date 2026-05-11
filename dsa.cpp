997c7632d21fc2e39de8655814af0576465999bb
Ayush <ayushanand1604@gmail.com>
2026-05-11 19:01:13 +0530

    leetcode 658 maxHeap

--- dsa.cpp
+++ dsa.cpp
@@ -2756,3 +2756,21 @@ public:
         return ans;
     }
 };
+658. Find K Closest Elements
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int>ans;
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<arr.size();i++){
+            pq.push({abs(arr[i]-x),arr[i]});
+            if(pq.size()>k) pq.pop();
+        }
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
