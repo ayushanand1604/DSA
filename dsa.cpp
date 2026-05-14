e1f00dabbab59fd6cb3f74e5673d857636a6ad07
Ayush <ayushanand1604@gmail.com>
2026-05-14 21:33:54 +0530

    leetcode 1929

--- dsa.cpp
+++ dsa.cpp
@@ -2809,3 +2809,18 @@ public:
         return ans;
     }
 };
+1929. Concatenation of Array
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans(2*n,0);
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            ans[i+n]=nums[i];
+        }
+        return ans;
+    }
+};
