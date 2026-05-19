5ab7d5340816f67f78757a3d4ab1568cee77f81d
Ayush <ayushanand1604@gmail.com>
2026-05-19 20:03:06 +0530

    graph1 number of provinces

--- dsa.cpp
+++ dsa.cpp
@@ -2861,3 +2861,27 @@ public:
         return ans;
     }
 };
+547. Number of Provinces
+class Solution {
+public:
+    void dfs(int i,vector<vector<int>>&adj,vector<bool>&vis){
+        vis[i]=true;
+        for(int j=0;j<adj[i].size();j++){
+            if(adj[i][j]==1&&!vis[j]){
+                dfs(j,adj,vis);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<bool>vis(n,false);
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,isConnected,vis);
+                count++;
+            }
+        }
+        return count;
+    }
+};
