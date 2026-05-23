66d59dabf9d218d2d636811e1ad404856d2abb88
Ayush <ayushanand1604@gmail.com>
2026-05-23 13:34:29 +0530

    merge intervals greedy 56

--- dsa.cpp
+++ dsa.cpp
@@ -2945,3 +2945,22 @@ public:
         return ans;
     }
 };
+56. Merge Intervals
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        for(int i=0;i<intervals.size();i++){
+            if(ans.empty()||ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                if(intervals[i][1]>ans.back()[1]){
+                    ans.back()[1]=intervals[i][1];
+                }
+            }
+        }
+        return ans;
+    }
+};
