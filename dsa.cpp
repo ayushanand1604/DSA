f3f57ce45972af5a6b080a28faec0bdedcb715c8
Ayush <ayushanand1604@gmail.com>
2026-05-29 02:02:37 +0530

    greedy leetcode 2483

--- dsa.cpp
+++ dsa.cpp
@@ -3139,3 +3139,26 @@ public:
         return ans==INT_MAX?-1:ans;
     }
 };
+2483. Minimum Penalty for a Shop
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int n=customers.size();
+        int penalty=count(customers.begin(),customers.end(),'Y');
+        int minmPenalty=penalty;
+        int minHour=0;
+        for(int i=0;i<n;i++){
+            if(customers[i]=='Y'){
+                penalty--;
+            }
+            else{
+                penalty++;
+            }
+            if(penalty<minmPenalty){
+                minmPenalty=penalty;
+                minHour=i+1;
+            }
+        }
+        return minHour;
+    }
+};
