d65ae5ed93d6c7716649a09f2e254f3cd27adeab
Ayush <ayushanand1604@gmail.com>
2026-05-29 22:35:36 +0530

    leetcode 989 carry movement

--- dsa.cpp
+++ dsa.cpp
@@ -3162,3 +3162,20 @@ public:
         return minHour;
     }
 };
+989. Add to Array-Form of Integer
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        if(k==0) return num;
+        for(int i=num.size()-1;i>=0;i--){
+            int sum=num[i]+k;
+            num[i]=sum%10;
+            k=sum/10;
+        }
+        while(k>0){
+            num.insert(num.begin(),k%10);
+            k=k/10;
+        }
+        return num;
+    }
+};
