c5466bd9f2aae9e9c08f74d918954bc8efcae48c
Ayush <ayushanand1604@gmail.com>
2026-07-13 23:20:26 +0530

    happy number leetcode 202

--- arrays.cpp
+++ arrays.cpp
@@ -426,7 +426,7 @@ class Solution {
     }
 };
 // Leaders in array
-lass Solution {
+class Solution {
     // Function to find the leaders in the array.
   public:
     vector<int> leaders(vector<int>& arr) {
