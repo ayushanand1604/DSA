12e67d88f1be33877e0354d9f9de86cd8bc8b449
Ayush <activeu281@gmail.com>
2026-07-31 16:58:07 +0530

    leetcode 3516

--- dsa.cpp
+++ dsa.cpp
@@ -3470,3 +3470,12 @@ public:
         return ans;
     }
 };
+3516. Find Closest Person 
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        if(abs(z-y)>abs(z-x)) return 1;
+        else if(abs(z-y)<abs(z-x)) return 2;
+        return 0;
+    }
+};
