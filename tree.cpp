7bd56bbbb7e5374866317c00c26bd17a5c788e8a
Ayush <activeu281@gmail.com>
2026-08-01 22:17:47 +0530

    maxm height of tree

--- tree.cpp
+++ tree.cpp
@@ -135,3 +135,13 @@ class Solution {
     }
 };
 
+// 104. Maximum Depth of Binary Tree
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+        int leftHeight=maxDepth(root->left);
+        int rightHeight=maxDepth(root->right);
+        return 1+max(leftHeight,rightHeight);
+    }
+};
