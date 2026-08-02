d2456110732c4850522b1edf175d891bbca0943c
Ayush <activeu281@gmail.com>
2026-08-02 21:57:18 +0530

    leetcode 4011

--- dsa.cpp
+++ dsa.cpp
@@ -3644,3 +3644,27 @@ public:
         return ans;
     }
 };
+
+// 4011. Count Subarrays With Even Odd Ratio I
+class Solution {
+public:
+    int countRatioSubarrays(vector<int>& nums, int a, int b) {
+        int n=nums.size();
+        long long ans=0;
+        for(int i=0;i<n;i++){
+            int even=0;
+            int odd=0;
+            for(int j=i;j<n;j++){
+                if(nums[j]%2==0)  even++;
+                else odd++;
+                if(odd>0){
+                    long long left=1LL * even * b;
+                    long long right=1LL * odd * a;
+                    if(left<=right) ans++;
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
