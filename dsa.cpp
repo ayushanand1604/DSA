4aa969b7f95cbfe0d4a09d0af05d846a34eadbd7
Ayush <activeu281@gmail.com>
2026-08-06 15:21:20 +0530

    logic based array

--- dsa.cpp
+++ dsa.cpp
@@ -3886,3 +3886,16 @@ class Solution {
         return decrementCount+maxDivideCount;
     }
 };
+
+1551. Minimum Operations to Make Array Equal
+class Solution {
+public:
+    int minOperations(int n) {
+        int target=n;
+        int count=0;
+        for(int i=0;i<n/2;i++){
+            count+=target-(2*i+1);
+        }
+        return count;
+    }
+};
