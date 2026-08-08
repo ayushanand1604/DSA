fcfeaa0893099af6119960e6922f0fe91212db64
Ayush <activeu281@gmail.com>
2026-08-08 08:14:03 +0530

    check balanced tree or not

--- tree.cpp
+++ tree.cpp
@@ -174,3 +174,20 @@ private:
         return 1 + max(leftHeight,rightHeight);
     }
 };
+
+// 110. Balanced Binary Tree leetcode 
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
+    }
+    int height (TreeNode *root) {
+        if(root == NULL) return 0;
+        int leftHeight= height(root->left);
+        if(leftHeight == -1) return -1;
+        int rightHeight = height(root->right);
+        if(rightHeight == -1) return -1;
+        if (abs(leftHeight - rightHeight) > 1) return -1;
+        return max (leftHeight,rightHeight) + 1;
+    }
+};
