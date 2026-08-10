fa2b2a2ad05534044e82ba3dfe0a845375cfd655
Ayush <activeu281@gmail.com>
2026-08-10 18:36:13 +0530

    sum of diagonal element

--- dsa.cpp
+++ dsa.cpp
@@ -3982,3 +3982,17 @@ public:
         return ans;
     }
 };
+
+1572. Matrix Diagonal Sum leetcode
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum=0;
+        int n=mat.size();
+        for(int i=0;i<n;i++){
+            sum += mat[i][i] + mat[i][n-i-1];
+        }
+        if (n%2) sum-= mat[n/2][n/2];
+        return sum;
+    }
+};
