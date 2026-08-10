d1dff41a0e413aeab102c609c06b0d8f4a0c6a25
Ayush <activeu281@gmail.com>
2026-08-10 18:38:13 +0530

    updated solution

--- dsa.cpp
+++ dsa.cpp
@@ -3992,7 +3992,7 @@ public:
         for(int i=0;i<n;i++){
             sum += mat[i][i] + mat[i][n-i-1];
         }
-        if (n%2) sum-= mat[n/2][n/2];
+        if (n%2==1) sum-= mat[n/2][n/2];
         return sum;
     }
 };
