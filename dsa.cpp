3b2312e6d8bce3dc0e834aab59cf78f0af79cba3
Ayush <activeu281@gmail.com>
2026-08-12 13:57:12 +0530

    binary search leetcode 1760

--- dsa.cpp
+++ dsa.cpp
@@ -4041,3 +4041,31 @@ public:
         return ans;
     }
 };
+
+1760. Minimum Limit of Balls in a Bag leetcode 
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int high=*max_element(nums.begin(),nums.end());
+        int low=1;
+        int ans=high;
+        while(low<high){
+            int penalty=(high+low)/2;
+            int ops = 0;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]>penalty){
+                    if(nums[i]%penalty==0) ops+=(nums[i]/penalty)-1;
+                    else ops+=nums[i]/penalty;
+                }    
+            }
+            if(ops<=maxOperations){
+                ans=penalty;
+                high=penalty;
+            }
+            else{
+                low=penalty+1;
+            }
+        }
+        return ans;
+    }
+};
