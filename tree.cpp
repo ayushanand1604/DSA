4bd79afa89894fe6059b821d27ba2267dde4ab77
Ayush <activeu281@gmail.com>
2026-08-12 14:53:56 +0530

    zig zag trasversal

--- tree.cpp
+++ tree.cpp
@@ -213,3 +213,44 @@ public:
         return (p->val==q->val)&& isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
+
+103. Binary Tree Zigzag Level Order Traversal leetcode
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        queue<TreeNode*>q;
+        if(root==NULL) return ans;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<int>level;
+            for(int i=0;i<n;i++){
+                TreeNode* temp=q.front();
+                level.push_back(temp->val);
+                q.pop();
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
+            }
+            ans.push_back(level);
+        }
+        for(int i=0;i<ans.size();i++){
+            if(i%2!=0){
+                reverse(ans[i].begin(),ans[i].end());
+            }
+        }
+        return ans;
+    }
+};
