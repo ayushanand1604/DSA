acdcb83f983c8f3fd806edb908524e17040dfc9b
Ayush <activeu281@gmail.com>
2026-08-14 11:08:25 +0530

    design parking system easy leetcode

--- dsa.cpp
+++ dsa.cpp
@@ -4132,3 +4132,29 @@ public:
         return ans;
     }
 };
+
+// 1603. Design Parking System 
+class ParkingSystem {
+public:
+    int b,m,s;
+    ParkingSystem(int big, int medium, int small) {
+        b=big;
+        m=medium;
+        s=small;
+    }
+    bool addCar(int carType) {
+        if(carType==1 && b>0){
+            b--;
+            return true;
+        }
+        else if(carType==2 && m>0){
+            m--;
+            return true;
+        }
+        else if(carType==3 && s>0){
+            s--;
+            return true;
+        }
+        return false;
+    }
+};
