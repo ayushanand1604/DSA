6538c7d17f4b257cf813b1423aa84077537b3272
Ayush <activeu281@gmail.com>
2026-08-14 10:54:18 +0530

    group anangram map of vector

--- dsa.cpp
+++ dsa.cpp
@@ -4109,3 +4109,26 @@ public:
         return answer;
     }
 };
+
+// 49. Group Anagrams leetcode 
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string,vector<string>>mpp;
+        for(int i=0;i<strs.size();i++){
+            string word=strs[i];
+            sort(word.begin(),word.end());
+            if(mpp.find(word)!=mpp.end()){
+                mpp[word].push_back(strs[i]);
+            }
+            else{
+                mpp[word]={strs[i]};
+            }
+        }
+        vector<vector<string>>ans;
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
