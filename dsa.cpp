8d73730362266e584b4b499c81f4f3921845369f
Ayush <activeu281@gmail.com>
2026-08-19 13:23:36 +0530

    leetcode 3471 maths approach logic

--- dsa.cpp
+++ dsa.cpp
@@ -4348,3 +4348,31 @@ public:
         return string(v.begin(),v.end());
     }
 };
+
+3471. Find the Largest Almost Missing Integer leetcode
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int ans = -1;
+        unordered_map<int,int>mpp;
+        int n = nums.size();
+        for(int i = 0; i < nums.size(); i++){
+            mpp[nums[i]]++;
+        }
+        if(k==1){
+            for(auto it:mpp){
+                if(it.second == 1) ans=max(ans,it.first);
+            }
+        }
+        if(k==n){
+            for(int i = 0;i < nums.size(); i++){
+                ans=max(ans,nums[i]);
+            }
+        }
+        if(k<n){
+            if(mpp[nums[0]]==1) ans=max(ans,nums[0]);
+            if(mpp[nums[n-1]]==1) ans=max(ans,nums[n-1]);
+        }
+        return ans;
+    }
+};
