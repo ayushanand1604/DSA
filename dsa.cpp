ae91467a626f237accea5437da628835d61a2355
Ayush <activeu281@gmail.com>
2026-08-20 15:10:12 +0530

    striver array leetcode 1752

--- dsa.cpp
+++ dsa.cpp
@@ -4414,3 +4414,18 @@ public:
         }
     }
 };
+
+1752. Check if Array Is Sorted and Rotated leetcode 
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]< nums[i-1]) count++;
+        }
+        if(nums[n-1]>nums[0]) count++;
+        if(count<=1) return true;
+        else return false;
+    }
+};
