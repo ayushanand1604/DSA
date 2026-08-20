19282f7045c6911d48f584c08ceb4efea0f9eb02
Ayush <activeu281@gmail.com>
2026-08-20 16:16:04 +0530

    leetcode 560 striver leetcode 560

--- dsa.cpp
+++ dsa.cpp
@@ -4452,3 +4452,21 @@ public:
         return ans;
     }
 };
+
+560. Subarray Sum Equals K leetcode 560
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+        int prefixSum=0;
+        int count=0;
+        mpp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            prefixSum+=nums[i];
+            int required=prefixSum-k;
+            count+=mpp[required];
+            mpp[prefixSum]++;
+        }
+        return count;
+    }
+};
