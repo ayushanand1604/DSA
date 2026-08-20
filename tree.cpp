9fa94469eebfbe1c26c90b3086e4519e6729cfa0
Ayush <activeu281@gmail.com>
2026-08-20 16:55:40 +0530

    tree top view

--- tree.cpp
+++ tree.cpp
@@ -254,3 +254,44 @@ public:
         return ans;
     }
 };
+
+Top View of Binary Tree GFG 
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+// time complexity O(n)
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        vector<int> ans;
+        if(root == nullptr) return ans;
+        map<int,int> mpp; // line , node
+        queue<pair<Node *,int>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            Node* node = it.first;
+            int line=it.second;
+            if(mpp.find(line)==mpp.end()) mpp[line]=node->data;
+            if(node->left != nullptr) q.push({node->left,line-1});
+            if(node->right != nullptr) q.push({node->right,line+1});
+        }
+        for(auto it: mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
