96e95f17ba1ccd863e4518e9b63f67c4e687901a
Ayush <activeu281@gmail.com>
2026-08-21 15:24:33 +0530

    leetcode 169 boyer moore algorithm

--- dsa.cpp
+++ dsa.cpp
@@ -4484,3 +4484,22 @@ public:
         return increasing || decreasing;
     }
 };
+
+
+169. Majority Element leetcode
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // boyer moore algorithm
+        int count=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                ans=nums[i];
+            }
+            if(nums[i]==ans) count++;
+            else count--;
+        }
+        return ans;
+    }
+};
