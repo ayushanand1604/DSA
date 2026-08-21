06671b38e0aa358864d307e33fb4b783edc99be4
Ayush <activeu281@gmail.com>
2026-08-21 15:15:22 +0530

    leetcode 896 boolean and or operator

--- dsa.cpp
+++ dsa.cpp
@@ -4470,3 +4470,17 @@ public:
         return count;
     }
 };
+
+896. Monotonic Array leetcode
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool increasing=true;
+        bool decreasing=true;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<nums[i-1]) increasing=false;
+            if(nums[i]>nums[i-1]) decreasing=false;
+        }
+        return increasing || decreasing;
+    }
+};
