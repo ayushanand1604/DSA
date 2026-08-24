b96379b86bfcca4e6e34186f17c2556315cb004f
Ayush <activeu281@gmail.com>
2026-08-24 17:27:39 +0530

    leetcode 1903 string

--- dsa.cpp
+++ dsa.cpp
@@ -4694,3 +4694,15 @@ public:
         return ans;
     }
 };
+
+1903. Largest Odd Number in String leetcode 
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for(int i=num.length()-1;i>=0;i--){
+            int digit=num[i]-'0';
+            if(digit%2 != 0) return num.substr(0,i+1);
+        }
+        return "";
+    }
+};
