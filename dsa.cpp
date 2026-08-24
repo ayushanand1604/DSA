2913ffdc1bff76bb9e97d628eb4df6961879a356
Ayush <activeu281@gmail.com>
2026-08-24 21:34:51 +0530

    game stone 8 leetcode 1872using dynamic

--- dsa.cpp
+++ dsa.cpp
@@ -4706,3 +4706,20 @@ public:
         return "";
     }
 };
+
+1872. Stone Game VIII
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n=stones.size();
+        vector<long long> prefix(n+1);
+        for(int i=0;i<n;i++){
+            prefix[i+1] = prefix[i] + stones[i];
+        }
+        long long best= prefix[n];
+        for(int i=n-1;i>=2;i--){
+            best=max(best,prefix[i]-best);
+        }
+        return best;
+    }
+};
