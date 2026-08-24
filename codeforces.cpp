d75c364197de3ad277ed52cb0da94526c768f98f
Ayush <activeu281@gmail.com>
2026-08-24 17:02:00 +0530

    cp 31 800 ques 2

--- codeforces.cpp
+++ codeforces.cpp
@@ -327,3 +327,26 @@ int main(){
         }    
     }
 }
+
+By ayush_anand123, contest: Educational Codeforces Round 158 (Rated for Div. 2), problem: (A) Line Trip, Accepted, #, Copy
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int ans=arr[0];
+        for(int i=1;i<n;i++){
+            ans=max(ans,arr[i]-arr[i-1]);
+        }
+        ans=max(ans,2*(x-arr[n-1]));
+        cout<<ans<<endl;
+    }
+    return 0;
+}
