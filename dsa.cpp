5c6eed791a967fcacc2cd47c60b361e6030e6b3b
Ayush <activeu281@gmail.com>
2026-08-26 12:08:07 +0530

    leetcode 2678 substring

--- dsa.cpp
+++ dsa.cpp
@@ -4773,3 +4773,15 @@ public:
         return sum;
     }
 };
+
+2678. Number of Senior Citizens leetcode
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0;
+        for(int i=0;i<details.size();i++){
+            if(stoi(details[i].substr(11,2)) >60) count++;
+        }
+        return count;
+    }
+};
