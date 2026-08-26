51a4a6862034dc82c4422048419b15315bc30f3e
Ayush <activeu281@gmail.com>
2026-08-26 11:52:02 +0530

    codeforces 800 question 3

--- codeforces.cpp
+++ codeforces.cpp
@@ -350,3 +350,27 @@ int main(){
     }
     return 0;
 }
+
+By ayush_anand123, contest: Codeforces Round 911 (Div. 2), problem: (A) Cover in Water, Accepted, #, Copy
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int count=0;
+        bool threeDots = false;
+        for(int i=0;i<n;i++){
+            if(s[i]=='.') count++;
+            if(i>=2 && s[i]=='.' && s[i-1]=='.' && s[i-2]=='.') threeDots=true;
+        }
+        if(threeDots) cout<<2<<endl;
+        else cout<<count<<endl;
+    }
+    return 0;
+}
