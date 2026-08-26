f5d6b8463442b5884a25111de4069ce291c54a81
Ayush <activeu281@gmail.com>
2026-08-26 18:54:31 +0530

    leetcode 2390

--- dsa.cpp
+++ dsa.cpp
@@ -4785,3 +4785,18 @@ public:
         return count;
     }
 };
+
+
+2390. Removing Stars From a String leetcode
+class Solution {
+public:
+    string removeStars(string s) {
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='*'){
+                s.erase(i-1,2);
+                i -= 2;
+            }    
+        }
+        return s;
+    }
+};
