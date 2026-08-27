ba03ba659754cbc139cbb0dee0f0b911e7e36456
Ayush <activeu281@gmail.com>
2026-08-27 22:39:22 +0530

    leetcode 2903 o(n^2)

--- dsa.cpp
+++ dsa.cpp
@@ -4815,3 +4815,19 @@ public:
         return ans;
     }
 };
+
+2903. Find Indices With Index and Value Difference I leetcode
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(abs(i-j)>=indexDifference && abs(nums[i]-nums[j])>=valueDifference){
+                    return {i,j};
+                }
+            }
+        }
+        return {-1,-1};
+    }
+};
