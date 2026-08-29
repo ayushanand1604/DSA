62c0ea039890657ae145c4ce1aa6f878841c7a6c
Ayush <activeu281@gmail.com>
2026-08-29 20:51:38 +0530

    tle rate 800 question number 5

--- codeforces.cpp
+++ codeforces.cpp
@@ -389,3 +389,23 @@ int main(){
     }
     return 0;
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        cout<<(arr[0]==1 ? "YES":"NO") <<endl;
+    }
+    return 0;
+}
