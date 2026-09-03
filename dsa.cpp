c8af7106fcae05080c7de47d8bbb349b798ce5b9
Ayush <activeu281@gmail.com>
2026-09-03 12:53:06 +0530

    leetcode 3876

--- dsa.cpp
+++ dsa.cpp
@@ -4999,6 +4999,7 @@ public:
     }
 };
 
+
 3038. Maximum Number of Operations With the Same Score I leetcode
 class Solution {
 public:
@@ -5015,5 +5016,24 @@ public:
             i += 2;
         }
         return count;
+    }
+};
+
+
+3876. Construct Uniform Parity Array II leetcode
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int minOdd = INT_MAX;
+        for(int i = 0;i < nums1.size();i++){
+            if(nums1[i]%2!=0){
+                minOdd = min(minOdd,nums1[i]);
+            }
+        }
+        if (minOdd == INT_MAX) return true;
+        for(int i = 0;i < nums1.size();i++){
+            if(nums1[i] % 2 == 0 && nums1[i] <= minOdd) return false;
+        }
+        return true;
     }
 };
