1b3df4f4aae6e84e02b6ec6aec2106dfc54cb644
Ayush <activeu281@gmail.com>
2026-09-05 13:16:38 +0530

    leetcode 452

--- dsa.cpp
+++ dsa.cpp
@@ -5054,4 +5054,21 @@ public:
     }
 };
 
-
+452. Minimum Number of Arrows to Burst Balloons leetcode
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(),points.end(),[](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+        int count = 1;
+        int arrow=points[0][1];
+        for(int i = 1;i < points.size();i++){
+            if(points[i][0]>arrow){
+                count++;
+                arrow=points[i][1];
+            }
+        }
+        return count;
+    }
+};
