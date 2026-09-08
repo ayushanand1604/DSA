e156b24dd00e3086adf68fa6e454ed5d515ccd01
Ayush <activeu281@gmail.com>
2026-09-08 22:16:34 +0530

    cp 31 rate 800 ques no 18

--- codeforces.cpp
+++ codeforces.cpp
@@ -632,3 +632,24 @@ int main(){
     }
     return 0;
 }
+
+By ayush_anand123, contest: Educational Codeforces Round 149 (Rated for Div. 2), problem: (A) Grasshopper on a Line, Accepted, #, Copy
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int x,k;
+        cin >>x>>k;
+        if(x % k !=0){
+            cout<<1<<"\n";
+            cout<<x<<"\n";
+        }
+        else{
+            cout<<2<<"\n";
+            cout<<x-1<<" "<<1<<"\n";
+        }
+    }
+    return 0;
+}
