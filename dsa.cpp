f241a7b944a6e7c930dd2cd77e29dce7923f76a6
Ayush <activeu281@gmail.com>
2026-09-11 00:06:04 +0530

    leetcode 3206

--- dsa.cpp
+++ dsa.cpp
@@ -5228,3 +5228,22 @@ public:
         return ans;
     }
 };
+
+
+3206. Alternating Groups I leetcode
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        int n = colors.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int left = (i - 1 + n) % n;
+            int right = (i + 1) % n;
+            if (colors[left] != colors[i] &&
+                colors[i] != colors[right]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
