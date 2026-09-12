4e0fa765567339d93840ab64c286bd3af01ef7b3
Ayush <activeu281@gmail.com>
2026-09-12 21:51:49 +0530

    leetcode 766

--- dsa.cpp
+++ dsa.cpp
@@ -5267,3 +5267,18 @@ public:
         return s.size();
     }
 };
+
+
+766. Toeplitz Matrix leetcode
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0;i < n-1;i++){
+            for(int j = 0;j < matrix[0].size()-1; j++){
+                if(matrix[i][j]!=matrix[i+1][j+1]) return false;
+            }
+        }
+        return true;
+    }
+};
