82bdfa39dd48fd595ad07e6719c675cec3f48045
Ayush <activeu281@gmail.com>
2026-09-19 21:00:11 +0530

    cp 31 rate 800 ques no 26

--- codeforces.cpp
+++ codeforces.cpp
@@ -777,3 +777,33 @@ int main() {
     }
     return 0;
 }
+
+
+By ayush_anand123, contest: Codeforces Round 853 (Div. 2), problem: (A) Serval and Mocha's Array, Accepted, #, Copy
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i = 0;i < n;i++){
+            cin >> arr[i];
+        }
+        int flag = 0;
+        for(int i = 0;i < n;i++){
+            for(int j = i+1;j < n;j++){
+                if(gcd(arr[i],arr[j])<=2){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 1) break;
+        }
+        if(flag == 1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
