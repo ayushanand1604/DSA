adc6077db2e7118eb2b9e85c4a6604009f04b6ce
Ayush <activeu281@gmail.com>
2026-09-23 19:51:06 +0530

    leetcode 841 keys and room dfs using graph

--- graph.cpp
+++ graph.cpp
@@ -46,3 +46,26 @@ class Solution {
         return ans;
     }
 };
+
+
+841. Keys and Rooms leetcode
+class Solution {
+public:
+    void dfs(vector<vector<int>>& rooms, vector<bool> &visited, int current){
+        visited[current] = 1;
+        for(int i = 0;i < rooms[current].size();i++){
+            if(!visited[rooms[current][i]]){
+                dfs(rooms,visited,rooms[current][i]);
+            }
+        }
+    }
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<bool> visited(n,0);
+        dfs(rooms,visited,0);
+        for(int i = 0 ;i < n;i++){
+            if(visited[i]==0) return false;
+        }
+        return true;
+    }
+};
