b7eaf51a6e4c456a936e9888607863223151c378
Ayush <activeu281@gmail.com>
2026-09-24 20:11:27 +0530

    leetcode 234 palindrome linked list striver solution

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -396,3 +396,52 @@ public:
         return prev;
     }
 };
+
+
+// Palindrome linked list leetcode
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseLinkedList(ListNode* head) {
+        ListNode *current=head;
+        ListNode *temp=nullptr;
+        ListNode *next;
+        while(current!=nullptr){
+            next=current->next;
+            current->next=temp;
+            temp=current;
+            current=next;
+        }
+        return temp;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL) return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next !=NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newHead = reverseLinkedList(slow->next);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second!=NULL){
+            if(first->val != second->val){
+                reverseLinkedList(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        return true;
+    }
+};
