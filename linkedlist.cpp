96f2309f9fc6bc1c945a78e9064f8f0806f75b79
Ayush <activeu281@gmail.com>
2026-09-24 20:10:16 +0530

    leetcode reverse linked list striver solution

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -369,3 +369,30 @@ public:
        return ans->next;
     }
 };
+
+// Reverse of linked list leetcode
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *temp=head;
+        ListNode *prev=nullptr;
+        ListNode *front;
+        while(temp!=nullptr){
+            front=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+    }
+};
