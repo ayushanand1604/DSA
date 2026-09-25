22c58c77555ae42a1490bb2c0e88a2ac86cd53e4
Ayush <activeu281@gmail.com>
2026-09-25 16:47:19 +0530

    leetcode 1791 center ogf graph

--- graph.cpp
+++ graph.cpp
@@ -69,3 +69,27 @@ public:
         return true;
     }
 };
+
+1791. Find Center of Star Graph leetcode 
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int n = 0;
+        int ans = 0;
+        for(int i = 0;i < edges.size();i++){
+            n = max(n,max(edges[i][0],edges[i][1]));
+        }
+        vector<int>indegree(n+1);
+        for(int i = 0;i < edges.size();i++){
+            indegree[edges[i][0]]++;
+            indegree[edges[i][1]]++;
+        }
+        for(int i = 0;i <indegree.size();i++){
+            if(indegree[i]==n-1){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
