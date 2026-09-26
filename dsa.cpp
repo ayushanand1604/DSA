2974fd811050476857eb571d05f56a162736422b
Ayush <activeu281@gmail.com>
2026-09-26 13:57:58 +0530

    leetcode 402 stack comaprison edge case

--- dsa.cpp
+++ dsa.cpp
@@ -5323,3 +5323,30 @@ public:
         return count;
     }
 };
+
+
+402. Remove K Digits
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string ans = "";
+        int n = num.length();
+        for(int i = 0; i < n; i++){
+            while(ans.length() > 0 && ans.back() > num[i] && k > 0){
+                ans.pop_back();
+                k--;
+            }
+            if(ans.length() > 0 || num[i]!='0'){
+                ans.push_back(num[i]);
+            }
+        }
+        while(k>0 && ans.length()>0){
+            ans.pop_back();
+            k--;
+        }
+        if(ans == ""){
+            return "0";
+        }
+        return ans;
+    }
+};
