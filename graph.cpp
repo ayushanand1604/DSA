5ab582deaa214fadeb65c7d1ca73ccf1f2ae4362
Ayush <activeu281@gmail.com>
2026-09-26 18:10:04 +0530

    leetcode 1971 graph dfs trasversal vvi

--- graph.cpp
+++ graph.cpp
@@ -93,3 +93,28 @@ public:
         return ans;
     }
 };
+
+
+1971. Find if Path Exists in Graph
+class Solution {
+public:
+    bool dfs(vector<vector<int>> &graph,vector<int> &visited,int current,int end){
+        if(current==end) return true;
+        visited[current] = 1;
+        for(int i = 0;i < graph[current].size();i++){
+            if(!visited[graph[current][i]]){
+                if(dfs(graph,visited,graph[current][i],end)) return true;
+            }    
+        }
+        return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        vector<vector<int>> graph(n);
+        vector<int> visited(n,0);
+        for(int i =0;i<edges.size();i++){
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+        }    
+        return dfs(graph,visited,source,destination);
+    }
+};
